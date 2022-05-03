926748cd2af8f79dc0ff2d765b1e99fd25499b77
Dillon Lucas <dill17lion@gmail.com>
2022-05-03 15:16:20 -0500

    Initial Commit

---
+++ Shared/CoreDataPracticeBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataPracticeBackgroundApp.swift
+//  Shared
+//
+//  Created by Dillon Lucas on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataPracticeBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
